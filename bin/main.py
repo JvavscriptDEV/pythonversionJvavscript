eed46807ce875ec4559aea8ad63fccad538566b3
ChengT-cmd <64889395+ChengT-cmd@users.noreply.github.com>
2020-07-10 14:23:58 +0800

    add bin
    This is python runtime,you can choose if you don't want it.

---
+++ bin/main.py
@@ -0,0 +1,6 @@
+print('===================JvavScript By Dr.HY_Z======================')
+print('|                     Joked By Dr.HY_Z                       |')
+print('|                 Powered By JvavScript Team                 |')
+print('|Please be patient ,We are developing python version function|')
+print('==============================================================')
+#We are learning python now,we can't make a python script now,please wait
